cf06a495c99f1cb6a968ac0bfbedbae0d6a94bd6
chethangs <chethanckm@hotmail.com>
2016-10-27 19:15:38 +0530

    Add files via upload

---
+++ pgm7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pgm7{
+public static void main(String args[]){
+
+String s;
+String var;
+System.out.println("Enter a string: ");
+Scanner sc=new Scanner(System.in);
+s=sc.next();
+System.out.println("The resverse string is: ");
+  for(int i=s.length()-1;i>=0;i--){
+   var= Character.toString(s.charAt(i));
+    System.out.print(var);
+  }
+}
+}
